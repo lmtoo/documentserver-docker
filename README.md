33a1c5d1faa016579411ebad78dc6eae2b381699
lmtoo <601235723@qq.com>
2018-10-01 00:20:44 +0800

    修改README

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ onlyoffice中文字体版docker镜像
 
 1. 将中文字体拷贝到chinese目录
 2. 运行build.sh命令
+3. 查看docker镜像：docker images 是否有lmtoo/documentserver
+4. 运行run.sh 
+5. 打开浏览器localhost:8888查看服务是否正常
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ sudo docker run -i -t -d -p 8888:80 \
     -v /app/onlyoffice/DocumentServer/logs:/var/log/onlyoffice  \
     -v /app/onlyoffice/DocumentServer/data:/var/www/onlyoffice/Data  \
     -v /app/onlyoffice/DocumentServer/lib:/var/lib/onlyoffice \
-    -v /app/onlyoffice/DocumentServer/db:/var/lib/postgresql  lubansoft/documentserver
+    -v /app/onlyoffice/DocumentServer/db:/var/lib/postgresql  lmtoo/documentserver
